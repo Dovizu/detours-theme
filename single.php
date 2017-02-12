4c3c725a273f7ff612eebdb8258955d37bc53799
Donny Reynolds <donnywith@gmail.com>
2017-02-11 17:34:16 -0800

    styles about the author correctly

--- single.php
+++ single.php
@@ -23,10 +23,10 @@ get_header(); ?>
 			<div class="about-author">
 				
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-sm-3 text-center">
 						<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
 					</div>
-					<div class="col-md-9">
+					<div class="col-sm-9">
 						<h4><?php echo get_the_author_meta( 'display_name' ); ?></h4>
 						<?php echo get_the_author_meta( 'description' ); ?>
 					</div>
